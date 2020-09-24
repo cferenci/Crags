905fdb79fd32c6495f14bb2ae97926df14915231
Chris Ferenci <chrisferenci@Chriss-MacBook-Pro.local>
2020-09-24 12:34:52 -0700

    Updated files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapSearchComponent } from './map-search/map-search.component';
+import { PopularCragsComponent } from './popular-crags/popular-crags.component';
+import { CragsProfileComponent } from './crags-profile/crags-profile.component';
+import { AddCragComponent } from './add-crag/add-crag.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'popular-crags', component: PopularCragsComponent },
+  { path: 'map-search', component: MapSearchComponent },
+  { path: 'crags-profile', component: CragsProfileComponent },
+  { path: 'add-crag', component: AddCragComponent },
+  { path: '**', component: PopularCragsComponent  }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NguiMapModule } from '@ngui/map';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MapSearchComponent } from './map-search/map-search.component';
+import { PopularCragsComponent } from './popular-crags/popular-crags.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { CragsProfileComponent } from './crags-profile/crags-profile.component';
+import { AddCragComponent } from './add-crag/add-crag.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapSearchComponent,
+    PopularCragsComponent,
+    AppHeaderComponent,
+    CragsProfileComponent,
+    AddCragComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: 'popular-crags', component: PopularCragsComponent},
+      {path: 'map-search', component: MapSearchComponent},
+    ]),
+    NguiMapModule.forRoot({apiUrl:'https://maps.googleapis.com/maps/api/js?key=AIzaSyCLOCFHGLaJvseES8jwd226ry-ti8xkpkE'}),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
